e6247fec632d9af0cdcd812112e7d46258af1b75
Rajeshroulo <rajraval017@gmail.com>
2020-05-01 18:29:47 +0530

    added feature to analyse the message

---
+++ src/main/java/MoodAnalyzer.java
@@ -0,0 +1,23 @@
+package com.moodanalyzer;
+
+public class MoodAnalyzer {
+    private String message;
+
+    public MoodAnalyzer(String message) {
+        this.message=message;
+    }
+
+    public String analyzeMood() throws MoodAnalysisException {
+        try {
+            if(message.length()==0)
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,"enter proper message");
+            if (message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }catch (NullPointerException ne) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL,"enter proper message");
+        }
+    }
+
+}
---
+++ src/test/java/MoodAnalyzerTest.java
@@ -0,0 +1,44 @@
+package com.moodanalyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+    private MoodAnalyzer moodAnalyzer;
+
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSAD() throws MoodAnalysisException {
+        moodAnalyzer = new MoodAnalyzer("I am in Sad mood");
+        String mood = moodAnalyzer.analyzeMood();
+        Assert.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void givenMessage_WhenHappy_ShouldReturnHappy() throws MoodAnalysisException {
+        moodAnalyzer = new MoodAnalyzer("I am in Happy mood");
+        String mood = moodAnalyzer.analyzeMood();
+        Assert.assertEquals("HAPPY",mood);
+    }
+
+    @Test
+    public void givenMessage_WhenNull_ShouldReturnInvalidMessage() throws MoodAnalysisException {
+        moodAnalyzer = new MoodAnalyzer(null);
+        try {
+            moodAnalyzer.analyzeMood();
+        } catch (MoodAnalysisException mae){
+            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL,mae.type);
+        }
+    }
+
+    @Test
+    public void givenMessage_WhenEmpty_ShouldReturnEmptyMessage() throws MoodAnalysisException {
+        moodAnalyzer = new MoodAnalyzer("");
+        try {
+            moodAnalyzer.analyzeMood();
+        } catch (MoodAnalysisException mae){
+            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY, mae.type);
+        }
+    }
+
+}
